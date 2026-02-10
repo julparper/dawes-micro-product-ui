4da4b16e3eea579c2f0b2d7c2bb2a467e5328b53
Julian <julian@yo.com>
2026-02-10 20:42:26 +0100

    Primera version

---
+++ src/main/java/es/etg/dawes/micro/product_ui/ProductUiApplication.java
@@ -0,0 +1,13 @@
+package es.etg.dawes.micro.product_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/etg/dawes/micro/product_ui/ProductUiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.etg.dawes.micro.product_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
